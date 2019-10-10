e337db5b5e1f5acfc1df82b3179673129d1a3c52
Superbiael <39025166+Superbiael@users.noreply.github.com>
2019-10-10 04:24:57 -0700

    Update fou.js

--- commands/misc/fou.js
+++ commands/misc/fou.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "fou",
     category: "misc",
-    description: "random fou out of 13 total images",
+    description: "random fou out of 14 total images",
     run: async (client, message, args) => {
-        number = 13;
+        number = 14;
         imageNumber = Math.floor (Math.random() * (number)) + 1;
         const msg = await message.channel.send ({files: ["./images/fou/" + imageNumber + ".png"]});
 
