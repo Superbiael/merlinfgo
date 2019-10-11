65a7d5b97de728f2eb44c76d9bde35f85c7b5482
Superbiael <39025166+Superbiael@users.noreply.github.com>
2019-10-11 13:42:06 -0700

    Add files via upload

---
+++ merlin.js
@@ -0,0 +1,92 @@
+const { Client, Collection } = require("discord.js");
+const { config } = require("dotenv");
+const fs = require("fs");
+
+const client = new Client({
+    disableEveryone: true
+})
+
+// Collections
+client.commands = new Collection();
+client.aliases = new Collection();
+
+client.categories = fs.readdirSync("./commands/");
+
+config({
+    path: __dirname + "/.env"
+});
+
+// Run the command loader
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.on("ready", () => {
+    console.log(`${client.user.username} is now online!`);
+
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "Fate Grand Order",
+            type: "STREAMING"
+        }
+    });
+})
+
+client.on("message", async message => {
+    const prefix = "mer!";
+    let chaldeaMas = (message.member.user.username) 
+
+    msg = message.content.toLowerCase();
+
+    // if (message.author.bot) return;
+    if (!message.guild) return;
+    // if (!message.content.startsWith(prefix)) return;
+
+    // If message.member is uncached, cache it.
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (cmd.length === 0) return;
+
+    // Get the command
+    let command = client.commands.get(cmd);
+    // If none is found, try to find it by alias
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    // If a command is finally found, run the command
+    if (command)
+        command.run(client, message, args);
+
+        // Commands that don't need a prefix
+
+        if(msg.startsWith ("hewwo merwin")) {
+            number = 3;
+            var random = Math.floor (Math.random() * (number)) + 1;
+            switch (random) {
+              case 1: message.channel.send ("Hewwo! <:fou:631864260900814877>"); break;
+              case 2: message.channel.send ("Hewwo! It's me, the dreamy sheep, Onii-san!"); break;
+              case 3: message.channel.send ("Hewwo " + [chaldeaMas] + "-kun! It is I, Merwin, everyone's trusted advisor.",); break;
+            }
+           }
+
+           if(msg.startsWith ("good night merlin")) {
+            return message.channel.send ("Are you tired? Then, rest well and have a sweet dream~" +[chaldeaMas]);
+           }
+
+           if(msg.startsWith ("oh merlin")) {
+            return message.channel.send ("I am he.");
+           }
+
+           if(msg.startsWith (prefix + "hex")) {
+               return message.channel.send ("#d96ddb");
+           }
+           
+        //    Test Command
+           if(msg.startsWith (prefix + "test")) {
+            return message.channel.send ("Don't panic, don't panic. You'll bite your tongue over the incantations.",);
+        }
+});
+
+client.login(process.env.TOKEN);
