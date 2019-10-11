050e589db0504f5057c1e3358a1ddb7fee045041
Superbiael <39025166+Superbiael@users.noreply.github.com>
2019-10-10 21:58:44 -0700

    Update bean.js

--- commands/misc/bean.js
+++ commands/misc/bean.js
@@ -1,9 +1,8 @@
 module.exports = {
     name: "bean",
     category: "misc",
-    description: "merbean...",
+    description: "Mer🅱ean...",
     run: async (client, message, args) => {
         const msg = await message.channel.send ({files:["./images/bean.png"]});
-
     }
 }
