422f3c2c03f125fc03715851b05139a62aa93337
Superbiael <39025166+Superbiael@users.noreply.github.com>
2019-10-24 18:17:00 -0700

    Update fou.js

--- commands/misc/fou.js
+++ commands/misc/fou.js
@@ -1,11 +1,11 @@
 module.exports = {
     name: "fou",
     category: "misc",
-    description: "random Fou image out of 14 images <:fou:631864260900814877>",
+    description: "random Fou image out of 26 images <:fou:631864260900814877>",
     run: async (client, message, args) => {
-        number = 14;
+        number = 26;
         imageNumber = Math.floor (Math.random() * (number)) + 1;
-        return message.channel.send ({files: ["./images/fou/" + imageNumber + ".png"]});
 
+        return message.channel.send ({files: ["./images/fou/" + imageNumber + ".png"]});
     }
 }
