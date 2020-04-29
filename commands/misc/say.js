8b54cb84fb2a5c2000069fef45851edc9ee4525d
Superbiael <39025166+Superbiael@users.noreply.github.com>
2020-04-28 23:27:17 -0700

    Update say.js

--- commands/misc/say.js
+++ commands/misc/say.js
@@ -8,9 +8,6 @@ module.exports = {
     usage: "mer!say <message> or mer!say embed <message>",
     run: (client, message, args) => {
         message.delete();
-//
-   let merlinchannel = message.guild.channels.find(channel => channel.name === 'general');
-   if(!merlinchannel) return;
        
         if (args.length < 0)
             return message.reply("Nothing to say?").then(m => m.delete(5000));
@@ -22,9 +19,9 @@ module.exports = {
                 .setDescription(args.slice(1).join(" "))
                 .setColor(roleColor === "#000000" ? "#ffffff" : roleColor);
 
-            message.merlinchannel.send(embed);
+            message.channel.send(embed);
         } else {
-            message.merlinchannel.send(args.join(" "));
+            message.channel.send(args.join(" "));
         }
     }
 }
