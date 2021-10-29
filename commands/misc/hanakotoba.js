10146d5df0ac265c51a5b7011b582c6d8c3ef1e0
Superbiael <39025166+Superbiael@users.noreply.github.com>
2021-10-28 22:43:45 -0700

    Update hanakotoba.js

--- commands/misc/hanakotoba.js
+++ commands/misc/hanakotoba.js
@@ -9,7 +9,7 @@ module.exports = {
         switch (random) {
             case 1: message.channel.send ("Let's talk about flowers. This flower's is \"noble\" and \"beautiful heart.\" I'll crown you with the Vertessen flower. Be happy, my king."); break;
             case 2: message.channel.send ("Let's talk about flowers. The language of flowers is \"milk\". Oh no, this isn't a joke from your mischievous brother Merlin. That really is the meaning for Lettuce. Isn't that amazing? lol"); break;
-            case 3: message.channel.send ("Let's talk about flowers. The language of flowers is \"revenge\" \"misanthropic,\" and \"pessimistic.\" Oh, I wonder who left me these Aconite flowers? Your journey of revenge has nothing to do with me.")
+            case 3: message.channel.send ("Let's talk about flowers. The language of flowers is \"revenge\" \"misanthropic,\" and \"pessimistic.\" Oh, I wonder who left me these Aconite flowers? Your journey of revenge has nothing to do with me."); break;
             case 4: message.channel.send ("Let's talk about flowers. The language of flowers is \"loyalty\" Hanai flowers bloom near water. I wonder if your regret is blooming in that lake, Lucius."); break;
         //    case 3: message.channel.send (""); break;
          }
